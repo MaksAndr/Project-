a1dc79c0dc78df0918811eb05d73acec03a342e3
Maksym Andreiko <119984723+MaksAndr@users.noreply.github.com>
2023-04-17 22:12:14 -0400

    Added files JS App

---
+++ app.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify, render_template 
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+engine = create_engine("sqlite:///us_shooting.db")
+ 
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/locations<br/>"
+        f"/api/v1.0/cases"
+    )
+
+@app.route("/index")
+def index_page():
+    return render_template("index.html")
+
+
+@app.route("/api/v1.0/cases")
+def get_cases():
+    conn = engine.connect()
+
+    df = pd.read_sql('SELECT * FROM GunViolence', conn)
+    data=df.to_dict(orient="records")
+    
+
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
---
+++ static/logic_1.js
@@ -0,0 +1,22 @@
+// Store our API endpoint as queryUrl.
+const url = "http://127.0.0.1:5000/api/v1.0/cases";
+// Perform a GET request to the query URL/
+d3.json(url).then(function (data) {
+    console.log(data);
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
